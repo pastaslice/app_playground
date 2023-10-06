3d6bcbf95d26cb61954e3a2af4762b69ab6eafe1
pastaslice <m.wolski1990@gmail.com>
2023-10-06 21:38:29 +0200

    “message”

---
+++ app_playground/app_playgroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  app_playgroundApp.swift
+//  app_playground
+//
+//  Created by Michał Wolski on 06/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct app_playgroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
