2362d6d64c72db187154ae185ef9045c616aef04
pastaslice <m.wolski1990@gmail.com>
2023-10-08 19:01:02 +0200

    “message”

--- app_playground/ContentView.swift
+++ app_playground/ContentView.swift
@@ -9,45 +9,58 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        
-        ZStack {
-            Color(.black)
-            VStack {
-                Image("malpa")
-                    .resizable()
-                    .cornerRadius(39.0)
-                    .aspectRatio(contentMode: .fit)
-                    .padding(.horizontal)
-                Text("MALPA PATRZY")
-                    .font(.largeTitle)
-                    .fontWeight(.bold)
-                    .foregroundColor(Color.green)
+        NavigationView {
+            ZStack {
+                Color(.black)
+                VStack {
+                    Image("malpa1")
+                        .resizable()
+                        .cornerRadius(39.0)
+                        .aspectRatio(contentMode: .fit)
+                        .padding(.horizontal)
+                    NavigationLink(destination: AnotherView()) {
+                        Text("AW")
+                            .font(.largeTitle)
+                            .fontWeight(.bold)
+                            .foregroundColor(Color.green)
+                            .padding(.leading)
+                            .background(Color.red)
+                            .cornerRadius(10)
+                }
+                
+                }
+                
+            }
+        }
+    }
+    
+    struct AnotherView: View {
+        var body: some View {
+            ZStack {
+                Color(.black)
+                VStack {
+                    Image("malpa2")
+                        .resizable()
+                        .cornerRadius(39.0)
+                        .aspectRatio(contentMode: .fit)
+                        .padding(.horizontal)
+                    NavigationLink(destination: ContentView()) {
+                        Text("KWARD")
+                            .font(.largeTitle)
+                            .fontWeight(.bold)
+                            .foregroundColor(Color.green)
+                            .padding(.leading)
+                            .background(Color.red)
+                            .cornerRadius(10)
+                    }
+                }
             }
         }
         
-        
-        
-        
-        
-
-//        Menu(/*@START_MENU_TOKEN@*/"Menu"/*@END_MENU_TOKEN@*/) {
-//            /*@START_MENU_TOKEN@*/Text("Menu Item 1")/*@END_MENU_TOKEN@*/
-//            /*@START_MENU_TOKEN@*/Text("Menu Item 2")/*@END_MENU_TOKEN@*/
-//            /*@START_MENU_TOKEN@*/Text("Menu Item 3")/*@END_MENU_TOKEN@*/
-//            Button(/*@START_MENU_TOKEN@*/"Button"/*@END_MENU_TOKEN@*/) {
-//                /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
-//            }
-//        }
-//        Stepper(value: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant(4)/*@END_MENU_TOKEN@*/, in: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Range@*/1...10/*@END_MENU_TOKEN@*/) {
-//            /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Label@*/Text("Stepper")/*@END_MENU_TOKEN@*/
-//        }
-//
-        
-    }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+        struct ContentView_Previews: PreviewProvider {
+            static var previews: some View {
+                ContentView()
+            }
+        }
     }
 }
